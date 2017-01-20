deea1de08d86c1e50ddd4252584a1ff60da430c5
Felix Back <felix.back@learnet.se>
2017-01-20 13:27:28 +0100

    first commit

---
+++ HelloWorld1/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld1
+{
+    class Class1
+    {
+        public void Run()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
